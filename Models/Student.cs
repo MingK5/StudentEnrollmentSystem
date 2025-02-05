d58b4c3b36554fff1ca19358fcae56bf909498f2
Liang Han Tan <lhtan@esrimalaysia.com.my>
2025-02-05 22:22:17 +0800

    Revert "try"
    This reverts commit cc7e77b7dccdc0165effcf6a4b334029abc3300d.

--- Models/Student.cs
+++ Models/Student.cs
@@ -22,8 +22,5 @@ namespace StudentEnrollmentSystem.Models
         [Required]
         [Column("password")]  // Matches "password" in SSMS
         public string Password { get; set; } = string.Empty;
-
-        [Column("test")]  // Matches "password" in SSMS
-        public string test { get; set; } = string.Empty;
     }
 }
